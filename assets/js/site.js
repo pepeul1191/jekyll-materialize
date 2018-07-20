d1c0f3904ea07c12117eaa1bc0b33c34a16a85bc
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-20 11:01:36 -0500

    Sidebar menu

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems);
+
 });
 
 // Or with jQuery
 
 $(document).ready(function(){
   $('.parallax').parallax();
+  $('.sidenav').sidenav();
 
   $(window).scroll(function(){
     if($(window).scrollTop() > 300){
   	  $('nav').addClass('white');
+      $('nav i.material-icons').addClass('color-primary');
       $('#nav-mobile li a').addClass('color-primary');
       $('.brand-logo img').attr('src',BASE_URL + '/assets/img/logo-coa.png');
     }else{
   	  $('nav').removeClass('white');
       $('#nav-mobile li a').removeClass('color-primary');
+      $('nav i.material-icons').removeClass('color-primary');
       $('.brand-logo img').attr('src',BASE_URL + '/assets/img/logo-coa-blanco.png');
     }
   })
